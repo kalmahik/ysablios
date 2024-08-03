afa54cc062979446304bb8e8ffc4608749ee9d27
kalmahik <kalmahik@yahoo.com>
2024-08-03 11:23:18 +0200

    Initial Commit

---
+++ ysablios/ysabliosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ysabliosApp.swift
+//  ysablios
+//
+//  Created by Murad Azimov on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct ysabliosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
