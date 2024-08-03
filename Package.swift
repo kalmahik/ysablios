708710ed98fa2a99a965d4bccde2c3b0dec4d9c5
kalmahik <kalmahik@yahoo.com>
2024-08-03 12:54:42 +0200

    [INIT-0][MA][Update git ignore]

--- Package.swift
+++ Package.swift
@@ -11,6 +11,9 @@ let package = Package(
             name: "ysablios",
             targets: ["ysablios"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.55.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
